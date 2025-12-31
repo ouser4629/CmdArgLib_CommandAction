adc5932e0bac90198814a199531e44cb1e5d8a88
Peter <p.summerland@gmail.com>
2025-12-31 12:03:07 -0500

    xxxx

--- Sources/Cmd_1_Simple/TopNode.swift
+++ Sources/Cmd_1_Simple/TopNode.swift
@@ -36,11 +36,11 @@ struct TopNode {
     private static let manpage: [ShowElement] = [
         .prologue(description: "Print a greeting or print some famous quotes."),
         .synopsis("SYNOPSIS", trailer: "Command"),
-        .lines("\nOPTIONS:"),
+        .lines("\nOPTIONS"),
         .parameter("h__help", "Show help information"),
         .parameter("t__tree", "Show command tree"),
         .parameter("m__manpage", "Print manpage mdoc code"),
-        .lines("\nSUBCOMMANDS:"),
+        .lines("\nSUBCOMMANDS"),
         .commandNode(Greet.command.asNode),
         .commandNode(Quotes.command.asNode),
     ]
